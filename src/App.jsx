2fd1760fcfd2d1d0236f067a78714e9b5a27e760
amirg76 <amirg76@gmail.com>
2023-12-07 12:26:53 +0200

    init front files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Login from '@pages/Login';
+
+function App() {
+  return (
+    <>
+      <h1 className='text-4xl font-bold'>React Boilerplate</h1>
+      <Login />
+    </>
+  );
+}
+
+export default App;
