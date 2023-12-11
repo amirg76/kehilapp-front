10a274d146612f0c2b14b8d5bf57b6f4d89a9200
Samir Khoury <samir@khoury.tk>
2023-12-11 23:15:08 +0200

    added todo comment

--- src/pages/Messages.jsx
+++ src/pages/Messages.jsx
@@ -8,6 +8,7 @@ const Messages = () => {
       {/* sidebar & content split side by side */}
       <div className="flex">
         <Sidebar />
+        {/* //TODO: Change to message cards components */}
         <h1 className="uppercase">Message Cards goes here Placeholder</h1>
       </div>
     </div>
