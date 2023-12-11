8540aeed196cb8a64baea6aa63f1136c8627ba29
Samir Khoury <samir@khoury.tk>
2023-12-12 01:08:19 +0200

    sidebar and sidebar items

---
+++ src/features/sidebar/components/Sidebar.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+// routes
+import { MESSAGES } from "@routes/routeConstants";
+// components
+import SidebarItem from "@features/sidebar/components/SidebarItem";
+//demo data
+import categories from "@demo-data/demoCategories.json";
+
+const Sidebar = () => {
+  return (
+    <aside className="h-screen w-80">
+      <nav className="h-full flex flex-col bg-white-500 border-e shadow-sm w1/2">
+        <h3 className="text-xl ms-3 mb-2 mt-2">קטגוריה</h3>
+        {/* nav links */}
+        <ul className="mb-5 ms-2">
+          {categories &&
+            categories.map((category) => (
+              <SidebarItem
+                key={category._id}
+                title={category.title}
+                color={category.color}
+                link={`${MESSAGES}/${category.link}`}
+                icon={category.icon}
+              />
+            ))}
+        </ul>
+
+        {/* New Message Button */}
+        <button
+          className="p-2 rounded-md mx-10 bg-primary-700 hover:bg-primary-600 text-white"
+          onClick={() => {
+            // TODO: open a new message model on click
+            console.log("Open New Message Modal");
+          }}
+        >
+          הוסף הודעה
+        </button>
+        <hr className="mt-5" />
+      </nav>
+    </aside>
+  );
+};
+
+export default Sidebar;
