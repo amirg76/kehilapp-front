552f5dc17870cb28f4442f1ac35b35992579a9b3
Samir Khoury <samir@khoury.tk>
2023-12-12 00:26:09 +0200

    adding routing to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+//routing
 import { Routes, Route, Navigate, useLocation } from "react-router-dom";
-
+import { ROOT, MESSAGES, MAIN_CATEGORY } from "@routes/routeConstants.js";
+//components
+//TODO: import NAVBAR
 import Messages from "./pages/Messages.jsx";
 
 const App = () => {
@@ -10,9 +13,26 @@ const App = () => {
       {/* //TODO: when is logged in redirect to the corresponding page, else redirect to login page */}
       <Routes>
         {/* Main Page - Redirect to the messages page if user */}
-        <Route path="/" element={<Navigate to="/messages" />} exact />
+        <Route
+          path={ROOT}
+          element={<Navigate to={MESSAGES + MAIN_CATEGORY} />}
+          exact
+        />
         {/* Routes */}
-        <Route path="messages" element={<Messages />} exact />
+        <Route
+          path={MESSAGES}
+          element={<Navigate to={MESSAGES + MAIN_CATEGORY} />}
+          exact
+        />
+        <Route
+          path={`${MESSAGES}/:categoryName`}
+          element={<Messages />}
+          exact
+        />
+
+        {/* //TODO: create a 404 Page for non-existing pages */}
+        {/* 404 Page for non-existing pages */}
+        {/* <Route path="*" element={} exact /> */}
       </Routes>
     </>
   );
