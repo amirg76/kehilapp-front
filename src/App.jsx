023ef888eca92c47cc11ff514b4cf7959218cb64
Samir Khoury <samir@khoury.tk>
2023-12-11 23:12:04 +0200

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+
+import Messages from "./pages/Messages.jsx";
 
 const App = () => {
   return (
     <>
-      <h1 className="text-4xl font-bold">React Boilerplate</h1>
+      <div className="h-[64px] bg-gray-500">NAVBAR COMPONENT</div>
+      {/* //TODO: when is logged in redirect to the corresponding page, else redirect to login page */}
+      <Routes>
+        {/* Main Page - Redirect to the messages page if user */}
+        <Route path="/" element={<Navigate to="/messages" />} exact />
+        {/* Routes */}
+        <Route path="messages" element={<Messages />} exact />
+      </Routes>
     </>
   );
 };
