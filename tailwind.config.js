ae4cddfe76d2966fdd6f8bedadee7d8e72e5fcff
Samir Khoury <samir@khoury.tk>
2023-12-11 19:36:10 +0200

    defined categories colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx}"
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        categoryBlue: "#6298CA",
+        categoryGreen: "#92C046",
+        categoryTurquoise: "#62B7CA",
+        categoryOrange: "#DE8989",
+        categoryPurple: "#9062CA",
+        categoryYellow: "#CAA062",
+        categoryDarkGreen: "#62CAA5",
+        categoryPink: "#CA62B3",
+      },
+    },
   },
   plugins: [],
-}
-
+};
