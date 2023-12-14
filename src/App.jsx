dbd3d85e6eb9d47ac64fbeacd93e5117d1034123
Samir Khoury <samir@khoury.tk>
2023-12-14 22:18:08 +0200

    moved routing to a separate routeConfig folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 //routing
-import { Routes, Route, Navigate, useLocation } from "react-router-dom";
-import { ROOT, MESSAGES, MAIN_CATEGORY } from "@routes/routeConstants.js";
+import { Routes, Route } from "react-router-dom";
+import routeConfig from "@routes/routeConfig";
 //components
 //TODO: import NAVBAR
-import Messages from "./pages/Messages.jsx";
 
 const App = () => {
   return (
@@ -12,27 +11,14 @@ const App = () => {
       <div className="h-[64px] bg-gray-500">NAVBAR COMPONENT</div>
       {/* //TODO: when is logged in redirect to the corresponding page, else redirect to login page */}
       <Routes>
-        {/* Main Page - Redirect to the messages page if user */}
-        <Route
-          path={ROOT}
-          element={<Navigate to={MESSAGES + MAIN_CATEGORY} />}
-          exact
-        />
-        {/* Routes */}
-        <Route
-          path={MESSAGES}
-          element={<Navigate to={MESSAGES + MAIN_CATEGORY} />}
-          exact
-        />
-        <Route
-          path={`${MESSAGES}/:categoryName`}
-          element={<Messages />}
-          exact
-        />
-
-        {/* //TODO: create a 404 Page for non-existing pages */}
-        {/* 404 Page for non-existing pages */}
-        {/* <Route path="*" element={} exact /> */}
+        {routeConfig.map((route, index) => (
+          <Route
+            key={index}
+            path={route.path}
+            element={route.element}
+            exact={route.exact}
+          />
+        ))}
       </Routes>
     </>
   );
