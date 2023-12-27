d67747464e6dda8ec64b04967b6f783cbde265cd
Dafna Bashan <dafnaba1@gmail.com>
2023-12-27 15:36:12 +0200

    add some note

--- src/store/slices/uiSlice.js
+++ src/store/slices/uiSlice.js
@@ -8,12 +8,12 @@ const uiSlice = createSlice({
     name: 'ui',
     initialState,
     reducers: {
+        //TODO: we need to choose if we want to use toggle or open/close actions.
         openModal(state) {
             state.isModalOpen = true
         },
         closeModal(state) {
             state.isModalOpen = false
-
         },
         toggleModal(state) {
             state.isModalOpen = !state.isModalOpen
