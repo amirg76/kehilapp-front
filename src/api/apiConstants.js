a746b1af3029de73f153a8a7cf6949de5a67248b
Samir Khoury <samir@khoury.tk>
2023-12-27 23:36:04 +0200

    adding api url constants

--- src/api/apiConstants.js
+++ src/api/apiConstants.js
@@ -1,2 +1,4 @@
 import { getBaseUrl } from "@utils/envUtils";
 export const BASE_URL = getBaseUrl();
+export const CATEGORY_URL = `${BASE_URL}api/categories`;
+export const MESSAGES_URL = `${BASE_URL}api/messages`;
