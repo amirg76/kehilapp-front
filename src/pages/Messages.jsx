5fea6e15a4ac341d5a73b0947c0d9e7487e07f8f
Samir Khoury <samir@khoury.tk>
2023-12-27 23:38:03 +0200

    dynamic messages fetch api

--- src/pages/Messages.jsx
+++ src/pages/Messages.jsx
@@ -1,16 +1,29 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+//components
 import Sidebar from "@features/sidebar/components/Sidebar";
-import MessageList from '../features/messages/components/MessageList'
+import MessageList from "../features/messages/components/MessageList";
 import HeroSection from "../features/heroSection/components/HeroSection";
-
+// api url
+import { MESSAGES_URL } from "@api/apiConstants.js";
 const Messages = () => {
-  let { categoryName } = useParams();
-
+  let { categoryId } = useParams();
+  const [messages, setMessages] = useState(null);
   //TODO: fetch messages based on the "categoryName" to the API
   useEffect(() => {
-    console.log(`fetching ${categoryName} messages!`);
-  }, [categoryName]);
+    //! demo fetch, to be used only as demo, replace with react query.
+    const getData = async () => {
+      if (!categoryId) {
+        categoryId = "all";
+      }
+      const response = await fetch(`${MESSAGES_URL}/category/${categoryId}`);
+      if (response.ok) {
+        let json = await response.json();
+        setMessages(json.data);
+      }
+    };
+    getData();
+  }, [categoryId]);
 
   return (
     <div className="flex flex-1 w-full bg-[#efefef]">
@@ -18,7 +31,7 @@ const Messages = () => {
       <Sidebar />
       <div className="flex flex-col">
         <HeroSection />
-        <MessageList />
+        <MessageList messages={messages} />
       </div>
     </div>
   );
