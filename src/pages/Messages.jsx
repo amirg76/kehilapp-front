0904a4de005cc589a1b4098ae80872fbb30a2fa1
Samir Khoury <samir@khoury.tk>
2023-12-29 18:22:42 +0200

    messages layout bug fix

--- src/pages/Messages.jsx
+++ src/pages/Messages.jsx
@@ -3,7 +3,8 @@ import { useParams } from "react-router-dom";
 //components
 import Sidebar from "@features/sidebar/components/Sidebar";
 import MessageList from "../features/messages/components/MessageList";
-import HeroSection from "../features/heroSection/components/HeroSection";
+import Hero from "../features/heroSection/components/Hero";
+
 // api url
 import {
   LATEST_MESSAGES_URL,
@@ -35,8 +36,8 @@ const Messages = () => {
     <div className="flex flex-1 w-full bg-[#efefef]">
       {/* sidebar & content split side by side */}
       <Sidebar />
-      <div className="flex flex-col">
-        <HeroSection />
+      <div className="w-full h-full">
+        <Hero />
         <MessageList messages={messages} />
       </div>
     </div>
