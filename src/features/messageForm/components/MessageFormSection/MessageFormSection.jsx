94b032178595079ff9b7ad17325673e9b8bf9a60
Dafna Bashan <dafnaba1@gmail.com>
2024-01-23 11:40:52 +0200

    add validate form function

--- src/features/messageForm/components/MessageFormSection/MessageFormSection.jsx
+++ src/features/messageForm/components/MessageFormSection/MessageFormSection.jsx
@@ -11,30 +11,32 @@ import { MESSAGES_URL } from "@api/apiConstants.js";
 
 const MessageFormSection = ({ categories, closeModal }) => {
 
-  const [formData, setFormData] = useState({
+  const [message, setMessage] = useState({
     categoryId: "",
     senderId: "099",
     title: "",
     text: "",
   });
 
+  const [error, setError] = useState({})
+
   const handleChange = (ev) => {
     const { name, value } = ev.target
-    setFormData({ ...formData, [name]: value });
+    setMessage({ ...message, [name]: value });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     // Add your form submission logic here
-    console.log("Form submitted:", formData);
+    console.log("Form submitted:", message);
 
-    const postData = async (formData) => {
+    const postData = async (message) => {
       const response = await fetch(MESSAGES_URL, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(formData),
+        body: JSON.stringify(message),
       });
       if (response.ok) {
         let json = await response.json();
@@ -42,9 +44,32 @@ const MessageFormSection = ({ categories, closeModal }) => {
         // setMessages(json.data);
       }
     };
-    postData(formData);
+    postData(message);
     closeModal();
   };
+
+  const validateForm = (formData) => {
+    const errors = {};
+  
+    // Title validation
+    if (!formData.title || formData.title.length < 2 || formData.title.length > 25) {
+      errors.title = 'Title must be between 2 and 25 characters';
+    }
+  
+    // Text validation
+    if (formData.text && formData.text.length > 250) {
+      errors.text = 'Text must be up to 250 characters';
+    }
+  
+    // Category validation
+    if (!formData.category) {
+      errors.category = 'Category is required';
+    }
+  
+    setError(errors)
+    return errors;
+  };
+  
 
   return (
     <form
@@ -54,14 +79,14 @@ const MessageFormSection = ({ categories, closeModal }) => {
 
       <SelectCmp options={categories} onChange={handleChange} defaultOption="קטגוריה" containerStyle="relative w-1/2 mb-5"/>
 
-      <InputCmp name="title" placeholder="כותרת" value={formData.title} onChange={handleChange}
+      <InputCmp name="title" placeholder="כותרת" value={message.title} onChange={handleChange}
         inputStyle="w-1/2 px-3 py-2 mb-5" />
 
       <TextareaCmp name="message-text" placeholder="כתיבת הודעה..."
         maxLength="250" rows="10" onChange={handleChange}
         style="w-full px-3 py-2" />
 
-      <InputCmp type="file" name="file" label="הוסף קובץ" formData={formData} onChange={handleChange}
+      <InputCmp type="file" name="file" label="הוסף קובץ" message={message} onChange={handleChange}
         children={<FileLinkSvg />} containerStyle="mb-4 flex flex-row-reverse justify-end items-center cursor-pointer"
         labelStyle="text-lg text-gray-400 underline underline-offset-4 mb-2 mr-2 cursor-pointer"
         inputStyle="hidden" />
