0b3de8ede36aefb7136b7bd59bb47b3dd558068d
Samir Khoury <samir@khoury.tk>
2024-01-28 16:54:06 +0200

    background image not showing aws bug

--- src/features/heroSection/components/HeroSection.jsx
+++ src/features/heroSection/components/HeroSection.jsx
@@ -3,11 +3,17 @@ import SearchMessages from "@features/searchMessages/components/SearchMessages/S
 
 const HeroSection = () => {
   return (
-    <div className="h-[270px] sm:h-[40vh] w-full bg-hero rounded-b-3xl bg-cover bg-center flex flex-col items-center mb-10 justify-end">
+    <div
+      className="h-[270px] sm:h-[40vh] w-full rounded-b-3xl bg-cover bg-center flex flex-col items-center mb-10 justify-end"
+      style={{
+        backgroundImage:
+          "url(https://i.ibb.co/0mfdBtk/42243380990100408272no.jpg)",
+      }}
+    >
       <h1 className="text-6xl font-semibold mb-2 text-white pt-8">
         הודעות הקיבוץ
       </h1>
-      <SearchMessages/>
+      <SearchMessages />
     </div>
   );
 };
