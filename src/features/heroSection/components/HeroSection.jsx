01520fcb8bc94051e40894af7595c463e683e67b
Samir Khoury <samir@khoury.tk>
2024-01-28 17:14:34 +0200

    aws image not showing bug fix, custom image url option

--- src/features/heroSection/components/HeroSection.jsx
+++ src/features/heroSection/components/HeroSection.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import SearchMessages from "@features/searchMessages/components/SearchMessages/SearchMessages";
 
 const HeroSection = () => {
+  // TODO: get custom kibbutz image url
+  let KibbutzCustomImage = "https://i.ibb.co/4V5rWFr/image2.png";
+
   return (
     <div
       className="h-[270px] sm:h-[40vh] w-full rounded-b-3xl bg-cover bg-center flex flex-col items-center mb-10 justify-end"
       style={{
-        backgroundImage:
-          "url(https://i.ibb.co/0mfdBtk/42243380990100408272no.jpg)",
+        backgroundImage: `url(${KibbutzCustomImage})`,
       }}
     >
       <h1 className="text-6xl font-semibold mb-2 text-white pt-8">
