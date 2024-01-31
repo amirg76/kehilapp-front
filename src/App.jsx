9cdcd989abe474da26d15a553b699cc0fc3ebadf
amirg76 <amirg76@gmail.com>
2024-01-31 11:31:41 +0200

    add login func with token

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 //routing
 import { Routes, Route, useLocation } from "react-router-dom";
 import routeConfig from "@routes/routeConfig";
 // routes constants
 import { LOGIN } from "@routes/routeConstants.js";
-
+// redux
+import { authActions } from "@store/slices/authSlice";
+import { useDispatch, useSelector } from "react-redux";
 //components
 import Header from "@components/Header/Header";
 
 const App = () => {
   const { pathname } = useLocation(); //* temp fix for hiding header on login screen
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    const user = localStorage.getItem("user");
+
+    if (token && user) {
+      dispatch(authActions.login(JSON.parse(user)));
+    }
+  }, []);
   return (
     <div className="w-screen flex flex-col ">
       {/* //TODO: when is logged in redirect to the corresponding page, else redirect to login page */}
