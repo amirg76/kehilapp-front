cc50ff2cc0d7ce3e44b187cdfdffe59f1bd71563
amirg76 <amirg76@gmail.com>
2024-02-12 15:13:04 +0200

    change message form width

--- src/features/messageForm/components/MessageForm/MessageForm.jsx
+++ src/features/messageForm/components/MessageForm/MessageForm.jsx
@@ -4,24 +4,27 @@ import MessageFormSection from "../MessageFormSection";
 import ModalDialog from "@components/ui/ModalDialog/ModalDialog";
 
 const MessageForm = ({ isModalOpen, toggleModal, categories }) => {
-
   const [isLoading, setIsLoading] = useState(false);
 
   const toggleLoading = (boolean) => {
-    setIsLoading(boolean)
-  }
+    setIsLoading(boolean);
+  };
 
   const closeModal = () => {
     toggleModal(false);
   };
-
-
+  //sm:w-[426px]
   return (
     <>
       <ModalDialog isOpen={isModalOpen} onClose={closeModal}>
-        <div className="bg-[#EFEFEF] w-screen sm:w-[426px] h-[728px] p-8 rounded-3xl shadow-md ">
+        <div className="bg-[#EFEFEF] w-screen sm:w-[600px] h-[728px] p-8 sm:rounded-3xl shadow-md ">
           {!isLoading && <MessageFormHeader closeModal={closeModal} />}
-          <MessageFormSection categories={categories} closeModal={closeModal} isLoading={isLoading} toggleLoading={toggleLoading}/>
+          <MessageFormSection
+            categories={categories}
+            closeModal={closeModal}
+            isLoading={isLoading}
+            toggleLoading={toggleLoading}
+          />
         </div>
       </ModalDialog>
     </>
