e2bf4e03e628d57c6d348fe5bf1298550bb2f1b2
Dafna Bashan <dafnaba1@gmail.com>
2024-03-11 15:03:28 +0200

    add login url

--- src/api/apiConstants.js
+++ src/api/apiConstants.js
@@ -3,5 +3,7 @@ export const BASE_URL = getBaseUrl();
 export const CATEGORY_URL = `${BASE_URL}api/categories`;
 export const MESSAGES_URL = `${BASE_URL}api/messages`;
 export const USERS_URL = `${BASE_URL}api/users`;
+export const AUTH_URL = `${BASE_URL}api/auth`;
 export const LATEST_MESSAGES_URL = `${MESSAGES_URL}/latest`;
 export const MESSAGES_BY_CATEGORY_URL = `${MESSAGES_URL}/category`;
+export const LOGIN_URL = `${AUTH_URL}/login`;
