ce9efbd5ec465eeca585b4022067b140bf97e1cf
amirg76 <amirg76@gmail.com>
2024-08-11 10:13:06 +0300

    add useNavbarHandler hook for handle navbar actions

--- src/components/Header/NavBar.jsx
+++ src/components/Header/NavBar.jsx
@@ -6,32 +6,13 @@ import { NavLink } from "react-router-dom";
 import NavBarContact from "./NavBarContact";
 import NavBarButton from "./NavBarButton";
 import Sidebar from "@features/sidebar/components/Sidebar";
+import useNavbarHandler from "@hooks/useNavbarHandler";
 import { ROOT } from "@routes/routeConstants";
 //redux use functions
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { uiActions } from "@store/slices/uiSlice";
 const NavBar = () => {
   const isModalOpen = useSelector((state) => state.ui.isModalOpen);
-  const dispatch = useDispatch();
-  /**
-   * Opens the navbar by dispatching the openModal action.
-   *
-   * @return {void} No return value.
-   */
-  const onOpenNavbar = () => {
-    dispatch(uiActions.openModal());
-  };
-
-  /**
-   * Closes the navbar after a short delay.
-   *
-   * @return {void} No return value.
-   */
-  const onCloseNavbar = () => {
-    setTimeout(() => {
-      dispatch(uiActions.closeModal());
-    }, 300);
-  };
 
   return (
     <nav className="flex h-24 items-center justify-between p-10 ">
@@ -47,13 +28,13 @@ const NavBar = () => {
         classes={`max-md:flex flex-column fixed left-0 top-0 bg-white opacity-90
              h-screen border-l-[1px]-[#ebebeb] z-30 transition-transform duration-600 pt-3
              ${isModalOpen ? "translate-x-0" : "translate-x-[-100%] md:flex"}`}
-        onCloseNavbar={onCloseNavbar}
+        onCloseNavbar={useNavbarHandler("close")}
         open={isModalOpen}
       />
 
       <FontAwesomeIcon
         className="md:hidden"
-        onClick={onOpenNavbar}
+        onClick={useNavbarHandler("open")}
         icon={faBars}
       />
     </nav>
