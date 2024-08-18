0205b4f52c2b022811456552bb8204824faa2477
amirg76 <amirg76@gmail.com>
2024-08-18 15:23:03 +0300

    delete double hooks path

--- vite.config.js
+++ vite.config.js
@@ -32,7 +32,7 @@ export default defineConfig({
       "@context": path.resolve(__dirname, "src/context"),
       "@hooks": path.resolve(__dirname, "src/hooks"),
       "@utils": path.resolve(__dirname, "src/utils"),
-      "@hooks": path.resolve(__dirname, "src/hooks"),
+
       "@assets": path.resolve(__dirname, "src/assets"),
       "@types": path.resolve(__dirname, "src/types"),
       "@api": path.resolve(__dirname, "src/api"),
